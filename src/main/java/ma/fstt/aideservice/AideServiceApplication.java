a6a411922600f5420d52031ca36eb9d05bf814e5
slord10 <soufianelamribah2000@gmail.com>
2025-01-20 14:25:34 +0100

    20/02

--- src/main/java/ma/fstt/aideservice/AideServiceApplication.java
+++ src/main/java/ma/fstt/aideservice/AideServiceApplication.java
@@ -2,8 +2,10 @@ package ma.fstt.aideservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AideServiceApplication {
 
     public static void main(String[] args) {
